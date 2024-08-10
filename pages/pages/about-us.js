52e40ee6b81ee2d68fde021210228974ad1237cd
Mohan Pratap Singh <pratapmohan27@gmail.com>
2024-08-11 04:10:26 +0530

    home module css error fixed

--- pages/pages/about-us.js
+++ pages/pages/about-us.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Layout from "../Layout/layout";
 import { BARREL_OPTIMIZATION_PREFIX } from "next/dist/shared/lib/constants";
 import Banner from "../Components/HomePage/Banner";
